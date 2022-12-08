b49f0f9fbd5b2c0d4cd4b3e41884b7d65c7ec3f7
Uinder Souza <>
2022-12-08 03:15:59 +0000

    06 - Entrada de dados

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var salario_bruto = 7500;
+const prompt = require('prompt-sync')();
+
+var nome = parseFloat(prompt('Digite seu nome: '));
+var salario_bruto = prompt('Digite seu salário bruto: ');
 var ir = salario_bruto * 0.10;
 var salario_liquido = salario_bruto - ir;
 console.log("Salário líquido: R$", salario_liquido);
