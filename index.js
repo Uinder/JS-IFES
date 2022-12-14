baca2b924e7a24999ab4134948fe2fbd4e8f4f21
Uinder Souza <>
2022-12-14 00:30:40 +0000

    07 - Comandos de Decisão Aninhados

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const prompt = require('prompt-sync')();
 
-var nome = parseFloat(prompt('Digite seu nome: '));
-var salario_bruto = prompt('Digite seu salário bruto: ');
+var nome = (prompt('Digite seu nome: '));
+var salario_bruto = parseFloat(prompt('Digite seu salário bruto: '));
+var dependentes = parseInt(prompt("Digite o número de dependentes"));
 
-if (salario_bruto > 0 && salario_bruto <= 1903.98) {
-  var ir = salario_bruto * 0.05;
-} else if (salario_bruto <= 2826.65) {
-  var ir = salario_bruto * o.o75;
+var renda_percapta = salario_bruto / (dependentes + 1);
+
+if (renda_percapta >= 500) {
+
+  if (salario_bruto > 0 && salario_bruto <= 1903.98) {
+    var ir = salario_bruto * 0.05;
+  } else if (salario_bruto <= 2826.65) {
+    var ir = salario_bruto * 0.075;
+  } else {
+    var ir = salario_bruto * 0.15;
+  }
 } else {
-  var ir = salario_bruto * 0.15;
+  var ir = 0;
 }
-
 var salario_liquido = salario_bruto - ir;
 console.log("Salário líquido: R$", salario_liquido);
