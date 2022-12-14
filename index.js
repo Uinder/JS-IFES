dd0a3bba6818a6ec39a04ed253e02657be67b1c4
Uinder Souza <>
2022-12-14 00:00:50 +0000

    07 - Comandos de Decisão

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const prompt = require('prompt-sync')();
 
 var nome = parseFloat(prompt('Digite seu nome: '));
 var salario_bruto = prompt('Digite seu salário bruto: ');
-var ir = salario_bruto * 0.10;
+
+if (salario_bruto > 0 && salario_bruto <= 1903.98) {
+  var ir = salario_bruto * 0.05;
+} else if (salario_bruto <= 2826.65) {
+  var ir = salario_bruto * o.o75;
+} else {
+  var ir = salario_bruto * 0.15;
+}
+
 var salario_liquido = salario_bruto - ir;
 console.log("Salário líquido: R$", salario_liquido);
